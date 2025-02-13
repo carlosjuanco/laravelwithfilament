edc87220a2cb851fa8d90b3f3ffc1e6d225fe97f
carlosjuanco <carlosjuancho328@gmail.com>
2025-02-12 18:07:30 -0600

    Clasificacion de elementos de navegacion de recursos Constructor de paneles - recursos - empezar

--- app/Filament/Resources/ConceptoResource.php
+++ app/Filament/Resources/ConceptoResource.php
@@ -18,6 +18,7 @@ class ConceptoResource extends Resource
     protected static ?string $model = Concepto::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?int $navigationSort = 5;
 
     public static function form(Form $form): Form
     {
