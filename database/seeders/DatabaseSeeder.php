7a00a219352fbe1e4cf91e57aedeecb4b71f5ef7
carlosjuanco <carlosjuancho328@gmail.com>
2025-08-14 19:05:19 -0600

    Agregar datos del usuario creador del sistema

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            UserSeeder::class,
             ProfesionistaSeeder::class,
             ExperienciasSeeder::class,
             FormacionesSeeder::class,
