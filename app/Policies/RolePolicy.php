147fa9c5d0346d5bf85bd147f8197258987956fb
carlosjuanco <carlosjuancho328@gmail.com>
2026-05-01 16:54:59 -0600

    LCF-5 Validar las reglas de negocio del componente roles

---
+++ app/Policies/RolePolicy.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class RolePolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return $user->role->name === "Creador del sistema";
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return $user->role->name === "Creador del sistema";
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Role $role): bool
+    {
+        return $user->role->name === "Creador del sistema";
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Role $role): bool
+    {
+        return $user->role->name === "Creador del sistema";
+    }
+}
