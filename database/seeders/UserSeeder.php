ff08474ddd7834c838477ab22dd01a7e61e54aca
carlosjuanco <carlosjuancho328@gmail.com>
2026-05-01 22:07:06 -0600

    LCF-6 Validar las reglas de negocio del componente usuarios

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
 use App\Models\User;
+use App\Models\Role;
 
 class UserSeeder extends Seeder
 {
@@ -15,10 +16,16 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+        $systemCreatorRole = new Role();
+        $systemCreatorRole->name = "Creador del sistema";
+        $systemCreatorRole->description = "Usuario principal que siempre existirá";
+        $systemCreatorRole->save();
+
         $user = new User();
         $user->name = 'Juan Carlos';
         $user->email = 'carlosjuancho328@gmail.com';
         $user->password = Hash::make('password');
+        $user->role_id = $systemCreatorRole->id;
 
         $user->save();
     }
